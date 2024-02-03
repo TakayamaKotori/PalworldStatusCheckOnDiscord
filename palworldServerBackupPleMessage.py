989cffbff482de61dbf64738b507f9356b245351
TakayamaKotori <takayamakotori@gmail.com>
2024-02-04 07:11:47 +0900

    メッセージ送信モジュール修正

--- palworldServerBackupPleMessage.py
+++ palworldServerBackupPleMessage.py
@@ -1,6 +1,6 @@
 import configparser
 from common import rcon, loggerUtil
-import asyncio
+import time
 
 config_ini = configparser.ConfigParser()
 config_ini.read("setting.ini", encoding="utf-8")
@@ -11,7 +11,7 @@ logger = loggerUtil.getLogger(__name__, logFilePath)
 
 
 def sendServerMessage(MessageText):
-    logger.debug("getServerVersion")
+    logger.debug("sendServerMessage")
     try:
         rcon.callPalworldRcon(f"/Broadcast {MessageText}")
     except Exception as e:
@@ -19,27 +19,27 @@ def sendServerMessage(MessageText):
         logger.error(e)
 
 
-async def main():
+def main():
     sendServerMessage("The server will restart after 15 minutes.")
-    await asyncio.sleep(60 * int(5))
+    time.sleep(60 * int(5))
 
     sendServerMessage("The server will restart after 10 minutes.")
-    await asyncio.sleep(60 * int(5))
+    time.sleep(60 * int(5))
 
     sendServerMessage("The server will restart after 5 minutes.")
-    await asyncio.sleep(60 * int(1))
+    time.sleep(60 * int(1))
 
     sendServerMessage("The server will restart after 4 minutes.")
-    await asyncio.sleep(60 * int(1))
+    time.sleep(60 * int(1))
 
     sendServerMessage("The server will restart after 3 minutes.")
-    await asyncio.sleep(60 * int(1))
+    time.sleep(60 * int(1))
 
     sendServerMessage("The server will restart after 2 minutes.")
-    await asyncio.sleep(60 * int(1))
+    time.sleep(60 * int(1))
 
     sendServerMessage("The server will restart after 1 minutes.")
-    await asyncio.sleep(60 * int(1))
+    time.sleep(60 * int(1))
 
 
 if __name__ == "__main__":
